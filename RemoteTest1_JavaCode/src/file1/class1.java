748c7bcd31b7f2d7c05bbbe552ca8083c96b8448
liv92 <74195329+liv92@users.noreply.github.com>
2020-11-11 13:19:04 -0500

    Added additional requirements to file1.

--- RemoteTest1_JavaCode/src/file1/class1.java
+++ RemoteTest1_JavaCode/src/file1/class1.java
@@ -2,6 +2,11 @@ package file1;
 
 public class class1 {
     public static void main(String[] args) {
+        
         System.out.println("Hello World!");
+        // mention your batch and group!
+        System.out.println("Hello Batch21!");
+        System.out.println("Hello Group31!");
+        
     }
 }
